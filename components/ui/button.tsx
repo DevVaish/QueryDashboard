b0a6cb77f9ea7a93280ab7375457fe2cb0d10572
Vaishnavi <vaishnavi2073@gmail.com>
2025-03-29 21:22:47 +0530

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,9 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-         result: "bg-white text-gray-700 hover:bg-gray-700 hover:text-white",
-        history: "bg-white text-gray-700 hover:bg-gray-700 hover:text-white",
-      },
+         },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
